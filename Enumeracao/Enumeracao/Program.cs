87127b6a1c267d1e9950495ce8f5de2f215e9c7d
Hamilton Limeira <hamiltondosanjos@gmail.com>
2020-12-18 07:43:19 -0300

    Enumerações e composições

---
+++ Enumeracao/Enumeracao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Enumeracao.Entities;
+using Enumeracao.Entities.Enums;
+
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+
+        }
+    }
+}
